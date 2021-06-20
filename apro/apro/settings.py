80176443f3115d582897b2167a31181325209046
sksrivatsava <60064405+sksrivatsava@users.noreply.github.com>
2021-06-20 13:43:01 +0530

    Add files via upload

--- apro/apro/settings.py
+++ apro/apro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '9v8mi$10bw9gu(0iizyl!e1irka8nbf%q$9b5165*2387zicun'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
