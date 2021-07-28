6e041100f263466838df0e6fe8e7545c6963dc14
sksrivatsava <sksrivatsava@gmail.com>
2021-07-28 09:33:14 +0530

    tc6

--- apro/urls.py
+++ apro/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from app import urls
+
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
